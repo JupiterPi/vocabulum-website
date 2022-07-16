7873c887a444802c623d16a1955a76b1ddf76af0
JupiterPi <jupiterpi@outlook.de>
2022-07-16 09:45:49 +0200

    added authentication for mongodb

--- server/src/newsletter/newsletter_email.service.ts
+++ server/src/newsletter/newsletter_email.service.ts
@@ -1,9 +1,12 @@
 import * as mongoDB from "mongodb";
+import fs from "fs";
 
 export const collections: { newsletterEmailsCollection?: mongoDB.Collection } = {};
 
 export async function connectToDatabase () {
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient("mongodb://localhost");
+    const connectionUrl = fs.readFileSync("mongodb_connect_url.txt").toString();
+    console.log(connectionUrl);
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(connectionUrl);
     await client.connect();
     const db: mongoDB.Db = client.db("vocabulum_website");
     const newsletterEmailsCollection: mongoDB.Collection = db.collection("newsletter_emails");
