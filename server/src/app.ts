11fbcc04a920016503613530018b89c8194ea895
JupiterPi <jupiterpi@outlook.de>
2023-04-30 01:51:18 +0200

    deploy to App Engine

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,7 @@ global.datastore = new Datastore();
 const app = require("express")();
 app.use(require("express").json());
 
-app.use("/newsletter", require("./newsletter/newsletter.controller"));
+app.use("/api/newsletter", require("./newsletter/newsletter.controller"));
 
 app.listen(8080, () => {
     console.log("Vocabulum Website running on port 8080!");
