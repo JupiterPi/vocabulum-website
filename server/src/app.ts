7831a17ac973a0206efd4047f07fad4d6ca0a340
JupiterPi <jupiterpi@outlook.de>
2023-04-29 14:55:44 +0200

    additional environment configurations for server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,15 @@
+import fs from "fs";
+import * as http from "http";
+import * as https from "https";
+
+/* configuration */
+type Environment = "dev" | "prod";
+const isProd = fs.readFileSync("configuration").toString().indexOf("prod") >= 0;
+
 const express = require("express");
 const app = express();
-const port = 63105;
+const httpPort = 63105;
+const httpsPort = 62105;
 
 app.use(express.json());
 
@@ -23,8 +32,27 @@ app.use("/api/newsletter", require("./newsletter/newsletter.controller"));
 
 app.use("/api/analyze", require("./analyze/analyze.controller"));
 
-app.listen(port, () => {
-    console.log(`Vocabulum Website server listening on port ${port}`);
+/*if (isProd) {
+    /!* --- for https *!/
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.vocabulum.de/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/www.vocabulum.de/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/www.vocabulum.de/chain.pem', 'utf8');
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+    console.log(credentials);
+
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(httpsPort, () => {
+        console.log(`Vocabulum Website server (https) listening on port ${httpsPort}`);
+    });
+}*/
+
+const httpServer = http.createServer(app);
+httpServer.listen(httpPort, () => {
+    console.log(`Vocabulum Website server listening on port ${httpPort}`);
 });
 
 require("./newsletter/newsletter_email.service").connectToDatabase();
